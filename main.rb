dc9c89101f6910d856b4f1f5137e70af1778c698
jenellywong <jenself_1995@yahoo.com>
2014-05-11 19:15:30 +0800

    created new repo, first commit

--- main.rb
+++ main.rb
@@ -1,10 +1,43 @@
 require 'sinatra'
 require './boot.rb'
+require './money_calculator.rb'
 
 # ROUTES FOR ADMIN SECTION
 get '/admin' do
   @products = Item.all
   erb :admin_index
+end
+
+get '/' do
+	@products = Item.all
+	@ten = @products.sample(10)
+	erb :home
+end
+
+get '/about' do
+	erb :about_page
+end
+
+get '/products' do
+	@products = Item.all
+	erb :products_page
+end
+
+get '/purchase/:id' do
+	@products = Item.all	
+	@product = Item.find(params[:id])
+	erb :buy
+end
+
+post '/purchase/:id' do
+	@product = Item.find(params[:id])
+	@something = MoneyCalculator.new(params[:ones], params[:fives], params[:tens], params[:twenties], params[:fifties], params[:hundreds], params[:five_hundreds], params[:thousands])
+	@givechange = @something.change(@product.price, params[:quantity])
+	@product.update_attributes(
+	quantity: @product.quantity - params[:quantity],
+	sold: params[:quantity] + @product.sold
+	)
+	erb :bought
 end
 
 get '/new_product' do
--- money_calculator.rb
+++ money_calculator.rb
@@ -1,12 +1,64 @@
 class MoneyCalculator
 
-  def initialize(ones, fives, tens, twenties, fifties, hundreds, five_hundreds, thousands)
-    # each parameter represents the quantity per denomination of money
-    # these parameters can be assigned to instance variables and used for computation
+	attr_accessor :ones, :fives, :tens, :twenties, :fifties, :hundreds, :five_hundreds, :thousands
 
-    # add a method called 'change' that takes in a parameter of how much an item costs
-    # and returns a hash of how much change is to be given
-    # the hash will use the denominations as keys and the amount per denomination as values
-    # i.e. {:fives => 1, fifties => 1, :hundreds => 3}
-  end
+	def initialize(ones, fives, tens, twenties, fifties, hundreds, five_hundreds, thousands)
+	@ones = ones.to_i
+	@fives = fives.to_i
+	@tens = tens.to_i
+	@twenties = twenties.to_i
+	@fifties = fifties.to_i
+	@hundreds = hundreds.to_i
+	@five_hundreds = five_hundreds.to_i
+	@thousands = thousands.to_i
+	# each parameter represents the quantity per denomination of money
+	# these parameters can be assigned to instance variables and used for computation
+	@sum = @ones + @fives*5 + @tens*10 + @twenties*20 + @fifties*50 + @hundreds*100 + @five_hundreds*500 + @thousands*1000
+    
+	end
+	
+	def sum
+		@sum
+	end
+
+
+
+	def change(item_price, number)
+	@item_price = item_price.to_i
+	@number = number.to_i
+	@total_price = @item_price * @number
+	
+
+		#if @total_price > @sum
+		#"Not enough money."
+		#else
+	@mchange = @total_price - @sum
+
+	a = @mchange
+	@nthousands, b = a.divmod(1000)
+	@nfivehundreds, c = b.divmod(500)
+	@nhundreds, d = c.divmod(100)
+	@nfifties, e = d.divmod(50)
+	@ntwenties, f = e.divmod(20)
+	@ntens, g = f.divmod(10)
+	@nfives, h = g.divmod(5)
+	@nones = h
+	
+	{ :thousands => @nthousands, :five_hundreds => @nfivehundreds, :hundreds => @nhundreds, :fifties => @nfifties, :twenties => @ntwenties, :tens => @ntens, :fives => @nfives, :ones => @nones}
+		#end
+
+	# add a method called 'change' that takes in a parameter of how much an item costs
+  	# and returns a hash of how much change is to be given
+  	# the hash will use the denominations as keys and the amount per denomination as values
+	# i.e. {:fives => 1, :fifties => 1, :hundreds => 3}
+	end
+
+	def mchange
+		@mchange
+	end
+
+	def totalprice
+		@totalprice
+	end
+
 end
